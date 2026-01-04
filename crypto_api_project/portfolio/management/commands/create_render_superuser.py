f600c96b0b270234d5a76244e26039d3309a3e38
Nouh <noohahmed600@gmail.com>
2026-01-04 11:55:21 +0300

    Add django management file for the render superuser creation. Also update render.yaml build command temporarly.

---
+++ crypto_api_project/portfolio/management/commands/create_render_superuser.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import get_user_model
+import os
+
+
+class Command(BaseCommand):
+    help = 'Create a superuser for Render deployment'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        username = os.getenv('RENDER_SUPERUSER_USERNAME')
+        email = os.getenv('RENDER_SUPERUSER_EMAIL')
+        password = os.getenv('RENDER_SUPERUSER_PASSWORD')
+
+        if not email or not password:
+            self.stdout.write(self.style.ERROR(
+                'RENDER_SUPERUSER_EMAIL and RENDER_SUPERUSER_PASSWORD environment are missing.'
+            ))
+            return
+        
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(self.style.WARNING(
+                f'Superuser with username "{username}" already exists.'
+            ))
+            return
+        
+        User.objects.create_superuser(username=username, email=email, password=password)
+        self.stdout.write(self.style.SUCCESS('Superuser created successfully.'))
